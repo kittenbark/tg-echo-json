b372de76132f0c57e5eb4216d0f9392f0e4718a2
kit <kittenbark@ya.ru>
2025-03-22 21:41:13 +0300

    refactor: add more comments and allow not only private messages

--- cmd/main.go
+++ cmd/main.go
@@ -9,20 +9,27 @@ import (
 
 func main() {
 	tg.NewFromEnv().
-		OnError(tg.OnErrorLog).
-		Filter(tg.OnPrivateMessage).
-		HandleCommand("/start", tg.CommonTextReply("Hii, this bot was made with https://github.com/kittenbark/tg.")).
-		Branch(tg.OnMessage, func(ctx context.Context, upd *tg.Update) error {
-			msgJson, err := json.MarshalIndent(upd.Message, "", "  ")
+		Scheduler(). // Optionally you could add Scheduler() to ensure no 429.
+		OnError(tg.OnErrorLog). // Let's log errors with slog.
+		Branch(tg.OnMessage, func(ctx context.Context, upd *tg.Update) error { // If tg update is message, send its contents.
+			msg := upd.Message
+			msgJson, err := json.MarshalIndent(msg, "", "  ")
 			if err != nil {
 				return err
 			}
 
-			_, err = tg.SendMessage(ctx, upd.Message.Chat.Id,
+			_, err = tg.SendMessage(ctx, msg.Chat.Id,
 				fmt.Sprintf("```json\n%s\n```", tg.EscapeParseMode(tg.ParseModeMarkdownV2, string(msgJson))),
-				&tg.OptSendMessage{ParseMode: tg.ParseModeMarkdownV2},
+				&tg.OptSendMessage{
+					ParseMode:       tg.ParseModeMarkdownV2,
+					ReplyParameters: &tg.ReplyParameters{MessageId: msg.MessageId},
+				},
 			)
 			return err
 		}).
+		Filter(tg.OnPrivateMessage). // Filter everything below, allow only private messages.
+		Command("/start", tg.CommonTextReply(helloMessage)). // Send greeting message.
 		Start()
 }
+
+const helloMessage = "Hii, this bot was made with https://github.com/kittenbark/tg."
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module tg-verbose-echo
 
 go 1.23
 
-require (
-	github.com/kittenbark/tg v0.0.0-20250214153129-c911add935d9
-)
+require github.com/kittenbark/tg v0.0.0-20250322070014-2bd40b9b2ddf
